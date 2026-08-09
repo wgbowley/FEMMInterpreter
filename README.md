0eecf5faff3cd870f15dd8371dc87ddbb00cbddc
William Bowley <wgrantbowley@gmail.com>
2026-08-09 17:48:34 +1000

    First successful parsing

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ A python library for interpreting FEMM solution files
 > [!important]
 > This tool will focus on the magnetic domain for now. And can be expanded in the future across all of FEMM solutions.
 
+<div align="center">
+  <img src="media/initial_a_potential_plot.png" alt="Magnetic vector potential Plot" style="max-width: 600px">
+    <p><em>Figure 1: Magnetic vector potential extracted from FEMM (.ans)</em></p>
+</div>
+
 ## Documentation
 
 All internal documentation can be found within this repo's [issues](https://github.com/wgbowley/FEMMInterpreter/issues).
--- src/FEMMInterpreter/parser.py
+++ src/FEMMInterpreter/parser.py
@@ -53,11 +53,27 @@ class ParseLineState:
     """Stores the state of the line parser"""
     index: int = 0
     section: str | None = None
+    block: str | None = None
     content: dict | None = None
+
+
+@dataclass(slots=True)
+class PointData:
+    """ Stores the state of a point """
+    x: float
+    y: float
+    a: float
+    boundary: int
 
 
 class ParseLines:
     """ Parsel lines for .ans file format """
+    BLOCK_PAIRS = {
+        "<BeginBdry>":    "<EndBdry>",
+        "<BeginBlock>":   "<EndBlock>",
+        "<BeginCircuit>": "<EndCircuit>"
+    }
+
     @classmethod
     def parse(cls, lines: list[str]) -> dict:
         """ Parses and extracts logic from raw text into structured results """
@@ -70,15 +86,49 @@ class ParseLines:
 
             is_section, name = cls._is_section(line)
             if is_section:
-                # Updates section based if identified
+                # Updates section if identified
                 state.section = name
+                state.content[name] = {}
 
-                # Checks for section value
-                raw_value = cls._extract_section_value(line)
+                if name.lower() == "solution":
+                    # Special Case for solution
+                    state.content[name] = []
+
+                # Checks for section value and casts the value into python primitives
+                is_value, raw_value = cls._extract_section_value(line)
                 value = Deserialize.cast(raw_value)
 
-                state.content[name] = {}
-                if value: state.content[name]["value"] = value
+                # Adds the value to the section
+                if is_value: state.content[name]["value"] = value
+
+                continue
+
+            if cls._is_new_blocks(line):
+                # Checks if its a block section and updates the block if identified
+                state.block = line.strip()
+                state.content[state.section][state.block] = {}
+
+                continue
+
+            if state.block and cls._is_close_block(line, state):
+                # Closes the block if closing block tag is identified
+                state.block = None
+                continue
+
+            if state.block:
+                # Extracts block and cases the values within the block
+                name, value = cls._extract_block_value(line, state)
+                value = Deserialize.cast(raw_value)
+
+                # Adds the name and value to the block and section
+                state.content[state.section][state.block][name] = value
+
+            if state.section.lower() == "solution":
+                values = line.split()
+                if len(values) == 4:
+                    # Records -> x | r, y | z, A, Boundary
+                    point = PointData(values[0], values[1], values[2], values[3])
+                    state.content[state.section].append(point)
 
         return state.content
 
@@ -99,10 +149,37 @@ class ParseLines:
         return False, ""
 
     @classmethod
-    def _extract_section_value(cls, line: str) -> str:
+    def _extract_section_value(cls, line: str) -> tuple[bool, str]:
         """ Extract section or subsection values """
         equal_sign = line.find("=")
         if equal_sign != -1:
-            # Returns the value if true
-            return line[equal_sign+1:]
-        return ""
+            # Returns the value if true and removes additional whitespaces
+            return True, line[equal_sign+1:].strip()
+        return False, ""
+
+    @classmethod
+    def _is_new_blocks(cls, line: str) -> bool:
+        """ Checks if its a block section """
+        if line in cls.BLOCK_PAIRS:
+            return True
+
+        return False
+
+    @classmethod
+    def _is_close_block(cls, line: str, state: ParseLineState) -> bool:
+        if line in cls.BLOCK_PAIRS[state.block]:
+            return True
+
+        return False
+
+    @classmethod
+    def _extract_block_value(cls, line: str, state: ParseLineState) -> tuple[str, str]:
+        """ Extract block name and value from line """
+        equal_sign = line.find("=")
+        if equal_sign == -1:
+            # Malformed block section found inline
+            msg = f"Malformed block section found in {line!r}, Index: {state.index}"
+            raise ParserError(cls.__name__, msg)
+
+        # Returns the name and value while removing additional whitespaces
+        return line[:equal_sign-1].strip(), line[equal_sign+1:].strip()
